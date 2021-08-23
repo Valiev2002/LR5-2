8de3d68fe113d5e624eb1ce9c4d91c9e4142c042
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-08-23 23:35:45 +0300

    Add files via upload

--- QueueList.h
+++ QueueList.h
@@ -1,18 +1,28 @@
 #pragma once
 #include "QueueList_iterator.h"
 #include <iterator>
+#include <iostream>
+#include <memory>
+template<class T>
+class QueueList;
+template<class T>
+        std::ostream &operator<<(std::ostream &out,const QueueList <T> &ql) {
+    for (auto *node = ql.first; node != nullptr; node = node->next) {
+        out <<  node->value;
+    }
+    return out;
+}
 template<class T>
         class QueueList {
-        private:
             struct node{
                 node *next;
-                node *previous;
-                int item;
+                T value;
             };
+    private:
             node *first;
             node *last;
+            friend std::ostream &operator<<<T>(std::ostream &out, const QueueList &ql);
             int size_;
-            node *ptr;
         public:
          class QueueList_iterator{
                 node* _pos;
@@ -21,78 +31,45 @@ template<class T>
                 using value_type = int; // Тип значения
                 using difference_type = int; // Тип расстояния
                 using pointer = int*; // Тип указателя
-                using reference = int&; // Тип ссылки
+                using reference = int&;// Тип ссылки
+                using const_reference = const int&;
                 explicit QueueList_iterator(node* pos):_pos(pos) {}
-             ~QueueList_iterator();
-            };
+             ~QueueList_iterator() = default;
+         };
          QueueList_iterator begin() const{return QueueList_iterator(first);};
          QueueList_iterator end() const{return QueueList_iterator(last);}
-            QueueList();
-            ~QueueList();
-            int front();
-            int back();
-            bool empty() const;
-            int size();
-            void pop_front();
-            void push_back(QueueList <T> &list, T value);
-            void clear();
+         QueueList():first(nullptr),last(nullptr),size_(0){};
+         ~QueueList(){clear();}
+         T front() {return first->value;}
+         T back() {return last->value;}
+            bool empty() const {return first==nullptr;}
+            int size() {return size_;}
+            void pop_front() {
+             if (first) {
+                 node* temp = first;
+                 first = first->next;
+                 delete temp;
+             }
+             if (!first) {
+                 last = nullptr;
+             }
+             --size_;
+            }
+            void push_back(const T &value) {
+             node *element = new node{nullptr, value};
+             if (empty()) {
+                 first = element;
+             }
+             else
+             {
+                 last->next = element;
+             }
+             last = element;
+             ++size_;
+            }
+            void clear() {
+             while (first) {
+                 pop_front();
+             }
+            }
         };
-
-//constructor
-template<class T>
-        QueueList<T>::QueueList(): ptr(nullptr),first(nullptr),last(nullptr),size_(0){}
-        //destructor
-        template<class T>
-                QueueList<T>::~QueueList() {
-                    delete this;
-                }
-                                template<class T>
-                                        int QueueList<T>::front() {
-                                            return first->item;
-                                        }
-                                        template<class T>
-                                                int QueueList<T>::back() {
-                                                    return last->item;
-                                                }
-                                                template<class T>
-                                                        bool QueueList<T>::empty() const {
-                                                            if(first==nullptr){
-                                                                return true;
-                                                            }
-                                                        }
-                                                        template<class T>
-                                                                int QueueList<T>::size() {
-                                                                    return size_;
-                                                                }
-                                                                template<class T>
-                                                                        void QueueList<T>::pop_front() {
-                                                                            if (first) {
-                                                                                node* temp = first;
-                                                                                first = first->next;
-                                                                                delete temp;
-                                                                            }
-                                                                            if (!first) {
-                                                                                last = nullptr;
-                                                                            }
-                                                                            --size_;
-                                                                        }
-                                                                        template<class T>
-                                                                                void QueueList<T>::clear() {
-                                                                                    while (first) {
-                                                                                        pop_front();
-                                                                                    }
-                                                                                }
-                                                                                template<class T>
-                                                                                        void QueueList<T>::push_back(QueueList <T> &list, T value) {
-                                                                                            node* elm = new node;
-                                                                                            if (!first) {
-                                                                                                first = elm;
-                                                                                                last = elm;
-                                                                                            }
-                                                                                            else {
-                                                                                                elm->previous = last;
-                                                                                                last->next = elm;
-                                                                                                last = elm;
-                                                                                            }
-                                                                                            size_++;
-                                                                                        }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include "QueueList.h"
 int main() {
+    int i;
     QueueList<int>lab{};
-    std::cout<<"Getting index from front: "<<std::endl;
-   lab.front();
-   std::cout<<"Getting index from back: "<<std::endl;
-   lab.back();
-   std::cout<<"Pop front: "<<std::endl;
-lab.pop_front();
-std::cout<<"Push back: "<<std::endl;
-lab.push_back(lab, 3);
-std::cout<<"Size: "<<std::endl;
-lab.size();
-std::cout<<"Begin iterator: "<<std::endl;
-lab.begin();
-std::cout<<"End iterator: "<<std::endl;
-lab.end();
-std::cout<<"Checking if it is empty: "<<std::endl;
-lab.empty();
-std::cout<<"Clear: "<<std::endl;
-lab.clear();
+    std::cout<<"Push back:"<<" ";
+    lab.push_back(2);
+    std::cout<<lab<<std::endl;
+    lab.push_back( 3);
+    std::cout<<lab<<std::endl;
+    lab.push_back( 4);
+    std::cout<<lab<<std::endl;
+    std::cout<<"Getting index from front:"<<" ";
+    lab.front();
+    std::cout<<lab<<std::endl;
+    std::cout<<"Getting index from back:"<<" ";
+    lab.back();
+    std::cout<<lab<<std::endl;
+    std::cout<<"Pop front: "<<std::endl;
+    lab.pop_front();
+    std::cout<<lab<<std::endl;
+    std::cout<<"Size: "<<lab.size()<<std::endl;
+    std::cout<<lab<<std::endl;
+    std::cout<<"Begin iterator: "<<std::endl;
+    lab.begin();
+    std::cout<<lab<<std::endl;
+    std::cout<<"End iterator: "<<std::endl;
+    lab.end();
+    std::cout<<lab<<std::endl;
+    std::cout<<"Checking if it is empty: "<<std::endl;
+    lab.empty();
+    std::cout<<lab<<std::endl;
+    std::cout<<"Clear: "<<std::endl;
+    lab.clear();
+    std::cout<<lab<<std::endl;
     return 0;
 }
