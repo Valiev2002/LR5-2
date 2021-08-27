59446b1fc6ef2dc239800abdccc432ffed689079
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-08-27 21:18:13 +0300

    Add files via upload

--- QueueList.h
+++ QueueList.h
@@ -1,75 +1,127 @@
 #pragma once
-#include "QueueList_iterator.h"
 #include <iterator>
 #include <iostream>
 #include <memory>
 template<class T>
 class QueueList;
+
 template<class T>
-        std::ostream &operator<<(std::ostream &out,const QueueList <T> &ql) {
+std::ostream &operator<<(std::ostream &out, const QueueList<T> &ql) {
     for (auto *node = ql.first; node != nullptr; node = node->next) {
-        out <<  node->value;
+        out << node->value << " ";
     }
+
     return out;
 }
+
 template<class T>
-        class QueueList {
-            struct node{
-                node *next;
-                T value;
-            };
-    private:
-            node *first;
-            node *last;
-            friend std::ostream &operator<<<T>(std::ostream &out, const QueueList &ql);
-            int size_;
-        public:
-         class QueueList_iterator{
-                node* _pos;
-            public:
-                using iterator_category = std::forward_iterator_tag;
-                using value_type = int; // Тип значения
-                using difference_type = int; // Тип расстояния
-                using pointer = int*; // Тип указателя
-                using reference = int&;// Тип ссылки
-                using const_reference = const int&;
-                explicit QueueList_iterator(node* pos):_pos(pos) {}
-             ~QueueList_iterator() = default;
-         };
-         QueueList_iterator begin() const{return QueueList_iterator(first);};
-         QueueList_iterator end() const{return QueueList_iterator(last);}
-         QueueList():first(nullptr),last(nullptr),size_(0){};
-         ~QueueList(){clear();}
-         T front() {return first->value;}
-         T back() {return last->value;}
-            bool empty() const {return first==nullptr;}
-            int size() {return size_;}
-            void pop_front() {
-             if (first) {
-                 node* temp = first;
-                 first = first->next;
-                 delete temp;
-             }
-             if (!first) {
-                 last = nullptr;
-             }
-             --size_;
-            }
-            void push_back(const T &value) {
-             node *element = new node{nullptr, value};
-             if (empty()) {
-                 first = element;
-             }
-             else
-             {
-                 last->next = element;
-             }
-             last = element;
-             ++size_;
-            }
-            void clear() {
-             while (first) {
-                 pop_front();
-             }
-            }
-        };
+class QueueList {
+    struct node {
+        node *next;
+        T value;
+    };
+private:
+    node *first;
+    node *last;
+    friend std::ostream &operator
+    <<<T>(
+    std::ostream &out,
+    const QueueList &ql
+    );
+    int size_;
+public:
+    class QueueList_iterator : public std::iterator<std::forward_iterator_tag, T> {
+        node *_pos;
+    public:
+        explicit QueueList_iterator(node *pos) : _pos(pos) {}
+
+        ~QueueList_iterator() = default;
+
+        QueueList_iterator &operator=(const QueueList_iterator rhs) {
+            _pos = rhs._pos;
+            return *this;
+        }
+
+        QueueList_iterator &operator++() {
+            _pos = _pos->next;
+            return *this;
+        }
+
+        QueueList_iterator operator++(int) {
+            QueueList_iterator temp = *this;
+            ++(*this);
+            return temp;
+        }
+
+        T operator*() const { return _pos->value; }
+
+        T &operator*() { return _pos->value; }
+
+        T *operator->() { return _pos; }
+
+        bool operator==(const QueueList_iterator &rhs) const { return _pos == rhs._pos; }
+
+        bool operator!=(const QueueList_iterator &rhs) const { return _pos != rhs._pos; }
+    };
+
+    const QueueList_iterator begin() const { return QueueList_iterator(first); };
+
+    const QueueList_iterator end() const {
+        return QueueList_iterator(nullptr);
+    }
+
+    QueueList_iterator begin() { return QueueList_iterator(first); }
+
+    QueueList_iterator end() {
+        return QueueList_iterator(nullptr);
+    }
+
+    QueueList() : first(nullptr), last(nullptr), size_(0) {};
+
+    ~QueueList() { clear(); }
+
+    T front() const { return first->value; }
+
+    T back() const { return last->value; }
+
+    bool empty() const { return first == nullptr; }
+
+    size_t size() const { return size_; }
+
+    void pop_front() {
+        if (first != nullptr) {
+            node *temp = first;
+            first = first->next;
+            delete temp;
+        }
+        if (first == nullptr) {
+            last = nullptr;
+        }
+        --size_;
+    }
+
+    void push_back(const T &value) {
+        node *element = new node{nullptr, value};
+        if (empty()) {
+            first = element;
+        } else {
+            last->next = element;
+        }
+        last = element;
+        ++size_;
+    }
+
+    void clear() {
+        while (first != nullptr) {
+            pop_front();
+        }
+    }
+};
+
+template<class T>
+void print(const QueueList<T> &lab) {
+    for (const auto &e: lab) {
+        std::cout << e << " <- ";
+    }
+    std::cout << "NULL" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,37 +1,84 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 #include "QueueList.h"
+
+
 int main() {
-    int i;
-    QueueList<int>lab{};
-    std::cout<<"Push back:"<<" ";
+    QueueList<int> lab;
+
+    std::cout << "Push back:" << " ";
+
     lab.push_back(2);
-    std::cout<<lab<<std::endl;
-    lab.push_back( 3);
-    std::cout<<lab<<std::endl;
-    lab.push_back( 4);
-    std::cout<<lab<<std::endl;
-    std::cout<<"Getting index from front:"<<" ";
+    std::cout << lab << std::endl;
+
+    lab.push_back(3);
+    std::cout << lab << std::endl;
+
+    lab.push_back(4);
+    std::cout << lab << std::endl;
+
+
+    std::cout<<"Print with range based for loop "<<std::endl;
+    print(lab);
+
+std::cout << "Let's check the number of even digits:"<<' ';
+    auto gg =std::count_if(lab.begin(),lab.end(),[](int i){return i % 2 == 0 ;});
+    if(gg){
+        std::cout<<gg<<std::endl;
+    }
+
+
+
+    /*int b;
+    std::cout<<"Please,enter your digit"<<std::endl;
+     std::cin>>b;
+     auto result=std::find(lab.begin(),lab.end(),b);
+     (result != lab.end())
+     ? std::cout << "lab contains " <<b << '\n'
+     : std::cout << "lab does not contain " << b << '\n';
+*/
+
+    auto biggest = std::max_element(lab.begin(),lab.end());
+    std::cout << "Max element is " << *biggest<<std::endl;
+
+    auto smallest = std::min_element(lab.begin(),lab.end());
+    std::cout << "Min element is " << *smallest<<std::endl;
+
+int m;
+std::cout <<" Please enter digit "<<std::endl;
+std::cin>>m;
+    std::for_each(lab.begin(), lab.end(), [m](int &n){  if (n > m) n += 10;});
+    print(lab);
+
+
+    int z;
+    std::cout <<" Please enter digit "<<std::endl;
+    std::cin>>m;
+    std::for_each(lab.begin(), lab.end(), [ z](int &n){  if (n < z) n -= 10;});
+    print(lab);
+
+    std::cout << "Pop front: " << std::endl;
+    std::cout << lab << std::endl;
+
+
+    std::cout << "Getting from front:" << " ";
     lab.front();
-    std::cout<<lab<<std::endl;
-    std::cout<<"Getting index from back:"<<" ";
-    lab.back();
-    std::cout<<lab<<std::endl;
-    std::cout<<"Pop front: "<<std::endl;
-    lab.pop_front();
-    std::cout<<lab<<std::endl;
-    std::cout<<"Size: "<<lab.size()<<std::endl;
-    std::cout<<lab<<std::endl;
-    std::cout<<"Begin iterator: "<<std::endl;
-    lab.begin();
-    std::cout<<lab<<std::endl;
-    std::cout<<"End iterator: "<<std::endl;
-    lab.end();
-    std::cout<<lab<<std::endl;
-    std::cout<<"Checking if it is empty: "<<std::endl;
-    lab.empty();
-    std::cout<<lab<<std::endl;
-    std::cout<<"Clear: "<<std::endl;
+    std::cout << lab.front() << std::endl;
+
+    std::cout << "Getting from back:" << " ";
+    std::cout << lab.back() << std::endl;
+
+
+    std::cout << "Size: " << lab.size() << std::endl;
+
+    std::cout << "Checking if it is empty: " << lab.empty() << std::endl;
+
+    std::cout << "Clear: " << std::endl;
     lab.clear();
-    std::cout<<lab<<std::endl;
+
     return 0;
 }
+
+
+
